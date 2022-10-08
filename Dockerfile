db22dbf0c2e9e9f87d075f25faa2f3779996bbe7
Sergey <sknyazev@plotpad.ru>
2022-10-09 00:02:31 +0300

    update encode algorithm & docker minor fix

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM python:3.9.14-alpine3.16
 
 WORKDIR /home/shortlink
 
+RUN apk add postgresql-dev
+
 COPY requirements.txt requirements.txt
 RUN pip3 install -r requirements.txt
 
--- services/ShortLinkGenerator.py
+++ services/ShortLinkGenerator.py
@@ -1,7 +1,11 @@
 from datetime import datetime
 
+from hashids import Hashids
+
 from entities.LinkEntity import LinkEntity
 import shortuuid
+
+hashids = Hashids(salt="this is my salt")
 
 
 def get_shortlink(long_link, name, author, deadline):
@@ -19,14 +23,19 @@ def get_shortlink(long_link, name, author, deadline):
 
 
 def generate_short_link(long_link, name, author, deadline):
-    short_link = shortuuid.uuid()
-    print(f"{long_link}: {short_link}")
-    LinkEntity.create(real_address=long_link, local_address=short_link, name=name, author=author, deadline=deadline)
-    return short_link
+    short_uuid = shortuuid.uuid()
+    print(f"{long_link}: {short_uuid}")
+    link_entity = LinkEntity.create(real_address=long_link, local_address=short_uuid, name=name, author=author,
+                                    deadline=deadline)
+    short_id = hashids.encode(link_entity.id)
+    link_entity.local_address = short_id
+    link_entity.save()
+    return short_id
 
 
 def get_long_link(short_link):
-    link_entity = LinkEntity.select().where(LinkEntity.local_address == short_link).get()
+    link_id = hashids.decode(short_link)
+    link_entity = LinkEntity.select().where(LinkEntity.id == link_id).get()
     present = datetime.now()
     if link_entity.deadline is not None and present > link_entity.deadline:
         print(link_entity.deadline)
